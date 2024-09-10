3d636434f907a48540019fde5b512bac4d27bfb9
Keszey Dániel <keszeyd@MacBook-Pro.local>
2024-09-10 17:07:15 +0200

    debug

--- crates/rbuilder/src/live_builder/order_input/txpool_fetcher.rs
+++ crates/rbuilder/src/live_builder/order_input/txpool_fetcher.rs
@@ -42,6 +42,7 @@ pub async fn subscribe_to_txpool_with_blobs(
         let mut stream = pin!(stream);
 
         while let Some(tx_hash) = stream.next().await {
+            println!("Some txn arrived");
             let start = Instant::now();
 
             let tx_with_blobs = match get_tx_with_blobs(tx_hash, &provider).await {
