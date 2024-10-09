7930a72ad6f8d3fad57b4c9200899fdf23360dcd
Brecht Devos <Brechtp.Devos@gmail.com>
2024-10-09 19:22:23 +0200

    remove unnecessary prints

--- crates/rbuilder/src/building/mod.rs
+++ crates/rbuilder/src/building/mod.rs
@@ -766,7 +766,6 @@ impl<Tracer: SimulationTracer> PartialBlock<Tracer> {
     ) -> eyre::Result<()> {
         let evm_config = EthEvmConfig::default();
         let mut db = state.new_db_ref();
-        println!("pre_block_beacon_root_contract_call");
         pre_block_beacon_root_contract_call(
             db.as_mut(),
             &evm_config,
@@ -775,7 +774,6 @@ impl<Tracer: SimulationTracer> PartialBlock<Tracer> {
             &ctx.block_env,
             ctx.attributes.parent_beacon_block_root(),
         )?;
-        println!("pre_block_blockhashes_contract_call");
         pre_block_blockhashes_contract_call(
             db.as_mut(),
             &evm_config,
@@ -784,7 +782,6 @@ impl<Tracer: SimulationTracer> PartialBlock<Tracer> {
             &ctx.block_env,
             ctx.attributes.parent,
         )?;
-        println!("merge_transitions");
         db.as_mut().merge_transitions(BundleRetention::Reverts);
         Ok(())
     }
