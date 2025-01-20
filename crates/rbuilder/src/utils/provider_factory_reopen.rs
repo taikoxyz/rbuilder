656a64dcd03f50482aaef7f2d8ee63be73a0a428
Brecht Devos <Brechtp.Devos@gmail.com>
2025-01-20 14:08:03 +0100

    disable testing mode

--- crates/rbuilder/src/utils/provider_factory_reopen.rs
+++ crates/rbuilder/src/utils/provider_factory_reopen.rs
@@ -35,7 +35,7 @@ impl<DB: Database + Clone> ProviderFactoryReopener<DB> {
             provider_factory: Arc::new(Mutex::new(provider_factory)),
             chain_spec,
             static_files_path,
-            testing_mode: true,
+            testing_mode: false,
         })
     }
 
@@ -48,7 +48,7 @@ impl<DB: Database + Clone> ProviderFactoryReopener<DB> {
             provider_factory: Arc::new(Mutex::new(provider_factory)),
             chain_spec,
             static_files_path,
-            testing_mode: true,
+            testing_mode: false,
         })
     }
 
